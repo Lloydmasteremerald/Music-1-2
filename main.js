5b0182ee9f334d8296bbd81b3c2fe2f3ebc2d57b
Lloydmasteremerald <110808538+Lloydmasteremerald@users.noreply.github.com>
2023-06-26 18:51:35 +1200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+Music1 = "";
+Music2 = "";
+
+function preload()
+{
+    Music1 = loadSound("music.mp3");
+    Music2 = loadSound("music2.mp3");
+}
+
+function setup()
+{
+    canvas = createCanvas(500, 450);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);
+}
+
